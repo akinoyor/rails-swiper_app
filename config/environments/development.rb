7412fb72c851c168e70e176f523833e1b6ad9eac
akinoyor <155416793+akinoyor@users.noreply.github.com>
2024-06-17 06:49:35 +0000

    事前準備

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -73,4 +73,5 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+  config.hosts << "47deeb0e71404552aed30cd10ab5aa47.vfs.cloud9.ap-northeast-1.amazonaws.com"
 end
